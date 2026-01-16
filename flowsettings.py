59bd5233837248548bd586804d51a751810ebb05
Oladapo Babajide <Oladapo.Babajide@moat.co.uk>
2026-01-16 00:34:21 +0000

    Rebrand application to Moat-Chat Bot and update favicon

--- flowsettings.py
+++ flowsettings.py
@@ -15,6 +15,8 @@ this_dir = Path(this_file).parent
 
 # change this if your app use a different name
 KH_PACKAGE_NAME = "kotaemon_app"
+KH_APP_NAME = "Moat-Chat Bot"
+KH_FAVICON_PATH = str(this_dir / "libs" / "ktem" / "ktem" / "assets" / "img" / "moat.jpg")
 
 KH_APP_VERSION = config("KH_APP_VERSION", None)
 if not KH_APP_VERSION:
--- libs/ktem/ktem/app.py
+++ libs/ktem/ktem/app.py
@@ -61,7 +61,9 @@ class BaseApp:
         with (dir_assets / "js" / "svg-pan-zoom.min.js").open() as fi:
             self._svg_js = fi.read()
 
-        self._favicon = str(dir_assets / "img" / "favicon.svg")
+        self._favicon = getattr(
+            settings, "KH_FAVICON_PATH", str(dir_assets / "img" / "favicon.svg")
+        )
 
         self.default_settings = SettingGroup(
             application=BaseSettingGroup(settings=settings.SETTINGS_APP),
